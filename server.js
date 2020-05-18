fa492c301196a9c4e0afb3a32553c0012bc0e8df
Akande Benji <adeoul001@gmail.com>
2020-05-18 00:47:51 -0700

    FixedBugs

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 3000;
 // mongodb+srv://Ben:queen1234@cluster0-rjlm3.mongodb.net/test?retryWrites=true&w=majority
 // mongodb://localhost:27017/contacts
 mongoose
-  .connect("mongodb://localhost:27017/contacts")
+  .connect("mongodb+srv://Ben:queen1234@cluster0-rjlm3.mongodb.net/test?retryWrites=true&w=majority")
   .then(() => console.log("Connected To DB"))
   .catch((err) => `Connection error:${err}`);
 
